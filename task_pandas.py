0fc1d2e7a74b837a7732f24ac8ed0da9c26b6681
Gronomix <Gronomix2013@gmail.com>
2023-04-06 14:19:11 +0300

    Задания из презентаций  на Пандас и CSV

--- task_pandas.py
+++ task_pandas.py
@@ -1,2 +1,19 @@
+'''Создать и прочитать CSV файл с помощью Pandas.
+Запросить у пользователя данные (имя, фамилия, возраст) и создать файл с этими значениями.
+Напишите программу, которая будет открывать определенный файл file и выводить на печать построчно последние строки
+в количестве lines (на всякий случай проверим, что задано положительное целое число).
+
+'''
+
 import pandas as pd
 
+df = pd.read_csv('exampl.csv')
+print(df)
+print(df.head())
+
+name = input('Введите имя ')
+sur = input('введите фамилию ')
+age = input('Введите возраст ')
+
+men = pd.DataFrame([[name, sur, age]], columns=['Name  ', 'Surname   ', 'Age  '])
+men.to_csv('exampl.csv', mode='a', index=False, header=False)
